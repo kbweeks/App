983c3ce28f3c9f05b2843c3215eeb55f58693f51
kbweeks <kbweeks@dhcp-v880-2-05155.1xwireless.unc.edu>
2014-11-13 09:50:44 -0500

    test

--- js/app2.js
+++ js/app2.js
@@ -3,13 +3,43 @@
 var htmlBuilder = "";
 $(document).ready(function() {
         
-        var parseAPPID = "x2dKlDuijtZ8PTLOFrQusYC0b1yyq1IHxvrJOiDA";
+      getList(CommentObject);
+});
+
+function getList(CommentObject){
+    console.log("getList" + CommentObject);
+    var query = new Parse.Query(CommentObject);
+    query.find({
+        success: function(results) {
+            console.log(results);
+            $.each(results, function( index, value ) {
+            console.log(results[index].attributes.cost);
+            htmlBuilder += '<a href="exampleevent.html">' + '<div class="box">' + '<div class="row">' + '<div class="small-10 columns">' + '<ul>' + results[index].attributes.name + '</br>' + results[index].attributes.venue + " : " + results[index].attributes.town + ", " + results[index].attributes.state +  '</br>' + results[index].attributes.day + " | " + results[index].attributes.time + '</br>'
+            + results[index].attributes.cost + '</ul>' + '</div>' + '<div class="small-2 columns">' + '</br>' + '<div class="friend-box">' + '<i class="fi-torso">' + " 3" + '</i>' + '</div>' + '</div>' + '</div>' + '</div>' + '</a>';
+});
+            $("#event").html(htmlBuilder);
+        },
+        
+        error: function(error) {
+        }
+        
+    });
+}
+
+
+
+    
+
+function saveEvent(){
+    console.log("saveEvent()");
+    
+      var parseAPPID = "x2dKlDuijtZ8PTLOFrQusYC0b1yyq1IHxvrJOiDA";
         var parseJSID = "s2AB8R5SL7GIhbYujjaRNQh2A9ZeXLnXwl9hzSL8";
         
         Parse.initialize(parseAPPID, parseJSID);
         var CommentObject = Parse.Object.extend("CommentObject");
         
-        getList(CommentObject);
+        //getList(CommentObject);
         
 	$("#commentForm").on("submit", function(e) {
 		e.preventDefault();
@@ -57,38 +87,20 @@ $(document).ready(function() {
             
         }
 
-    });
-
-function getList(CommentObject){
-    console.log("getList" + CommentObject);
-    var query = new Parse.Query(CommentObject);
-    query.find({
-        success: function(results) {
-            console.log(results);
-            $.each(results, function( index, value ) {
-            console.log(results[index].attributes.cost);
-            htmlBuilder += '<a href="exampleevent.html">' + '<div class="box">' + '<div class="row">' + '<div class="small-10 columns">' + '<ul>' + results[index].attributes.name + '</br>' + results[index].attributes.venue + " : " + results[index].attributes.town + ", " + results[index].attributes.state +  '</br>' + results[index].attributes.day + " | " + results[index].attributes.time + '</br>'
-            + results[index].attributes.cost + '</ul>' + '</div>' + '<div class="small-2 columns">' + '</br>' + '<div class="friend-box">' + '<i class="fi-torso">' + " 3" + '</i>' + '</div>' + '</div>' + '</div>' + '</div>' + '</a>';
-});
-            $("#event").html(htmlBuilder);
-        },
-        
-        error: function(error) {
-        }
-        
-    });
+        });
 }
-    
-    
+        
 
 
 function capturePhoto() {
+    console.log("capturePhoto()");
     navigator.camera.getPicture(uploadPhoto,null,{sourceType:2,quality:60,});
 }
 
 
 function uploadPhoto(data){
     //send file to server
+    console.log("uploadPhoto()");
    
    cameraPic.src = data;
         navigator.notification.alert(
