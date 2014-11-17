74d2fbe7f41051f17cbf76f5d6287eac64a7e96d
kbweeks <kbweeks@dhcp-v880-5-00257.1xwireless.unc.edu>
2014-11-17 14:49:35 -0500

    geopoint

---
+++ app.js
@@ -0,0 +1,112 @@
+//Parse.initialize("x2dKlDuijtZ8PTLOFrQusYC0b1yyq1IHxvrJOiDA", "s2AB8R5SL7GIhbYujjaRNQh2A9ZeXLnXwl9hzSL8");
+var htmlBuilder = "";
+$(document).ready(function() {
+        
+        var parseAPPID = "x2dKlDuijtZ8PTLOFrQusYC0b1yyq1IHxvrJOiDA";
+        var parseJSID = "s2AB8R5SL7GIhbYujjaRNQh2A9ZeXLnXwl9hzSL8";
+        
+        Parse.initialize(parseAPPID, parseJSID);
+        var CommentObject = Parse.Object.extend("CommentObject");
+        
+	
+        getList(CommentObject);
+	
+        
+	$("#commentForm").on("submit", function(e) {
+		e.preventDefault();
+		
+		console.log("Handling the submit");
+		//add error handling here
+		//gather the form data
+
+		var data = {};
+		data.name = $("#name").val();
+		data.venue = $("#venue").val();
+		data.town = $("#town").val();
+		data.state = $("#state").val();
+                data.day = $("#day").val();
+		data.time = $("#time").val();
+		data.cost = $("#cost").val();
+                data.description=$("#description").val();
+		//placeObject.set("location", point);
+                
+                var comment = new CommentObject();
+                comment.save(data,{
+                    success:function() {
+                        console.log("Success");
+                        alert("You've created an event, now invite your friends!");
+                    },
+                    error:function(e) {
+                        console.dir(e);
+                    }
+                });
+	
+    });
+
+});
+
+
+
+function getList(CommentObject){
+    console.log("getList" + CommentObject);
+    var query = new Parse.Query(CommentObject);
+    query.find({
+        success: function(results) {
+            console.log(results);
+            $.each(results, function( index, value ) {
+            console.log(results[index].attributes.cost);
+            htmlBuilder +=  '<div class="box">' + '<div class="row">' + '<div class="small-10 columns">' + '<ul>' + results[index].attributes.name + '</br>' + results[index].attributes.venue + " : " + results[index].attributes.town + ", " + results[index].attributes.state +  '</br>' + results[index].attributes.day + " | " + results[index].attributes.time + '</br>'
+            + results[index].attributes.cost + '</ul>' + '</div>' + '<button id="element1" onclick="javascript:changeText(1)">Not Going</button>'+ '<div class="small-2 columns">' + '</br></br>' + '<div class="friend-box">' + '<i class="fi-torso">' + " 3" + '</i>' + '</div>' + '</div>' + '</div>' + '</div>' + '</a>';
+});
+            $("#event").html(htmlBuilder);
+        },
+        
+        error: function(error) {
+        }
+	
+	
+        
+    });
+    
+function changeText(idElement) {
+    var element = document.getElementById('element' + idElement);
+    if (idElement === 1) {
+        if (element.innerHTML === 'Not Going') element.innerHTML = 'Going';
+        else {
+            element.innerHTML = 'Not Going';
+        }
+    }
+}
+
+document.addEventListener("deviceready", onDeviceReady, false);
+
+function onDeviceReady(){
+    console.log("onDeviceReady()");
+}
+
+
+function capturePhoto() {
+    navigator.camera.getPicture(uploadPhoto,null,{sourceType:2,quality:60,});
+}
+
+
+function uploadPhoto(data){
+    //send file to server
+   
+   cameraPic.src = data;
+        navigator.notification.alert(
+                'Your photo has been uploaded', //message
+                okay,                           //callback
+            'Photo uploaded',               //title
+            'OK'                            //buttonName
+        );
+}
+
+function okay (){
+        //Do Something
+}
+
+
+
+}
+
---
+++ apptest.js
@@ -0,0 +1,114 @@
+
+var htmlBuilder = "";
+var currentLocation;
+$(document).ready(function() {
+        
+        var parseAPPID = "x2dKlDuijtZ8PTLOFrQusYC0b1yyq1IHxvrJOiDA";
+        var parseJSID = "s2AB8R5SL7GIhbYujjaRNQh2A9ZeXLnXwl9hzSL8";
+        
+        Parse.initialize(parseAPPID, parseJSID);
+        var CommentObject = Parse.Object.extend("CommentObject");
+	
+	getList(CommentObject);
+	
+	
+	 if($("#submitEventBtn").length === 1) {
+		currentLocation=null;
+		navigator.geolocation.getCurrentPosition(function(pos) {
+			//store the long/lat
+			currentLocation = {longitude:pos.coords.longitude, latitude:pos.coords.latitude};
+			$("#sumbitEventBtn").removeAttr("disabled");
+		}, function(err) {
+			//Since geolocation failed, we can't allow the user to submit
+			alert("Sorry, we couldn't find your location. Try connecting to Wi-Fi or try again later.");
+		});
+
+    }
+	
+	
+	$("#commentForm").on("submit", function(e) {
+	e.preventDefault();
+ 
+	//get values
+	var name = $("#name").val();
+	var venue = $("#venue").val();
+	var town = $("#town").val();
+	var state = $("#state").val();
+        var day = $("#day").val();
+	var time = $("#time").val();
+	var cost = $("#cost").val();
+        var description=$("#description").val();
+ 
+	var comment = new CommentObject();
+	var point = new Parse.GeoPoint({latitude: currentLocation.latitude, longitude: currentLocation.longitude});
+	comment.save(
+			{
+				name:name,
+				venue:venue,
+				town:town,
+				state:state,
+				day:day,
+				time:time,
+				cost:cost,
+				description:description,
+				location:point
+			},{
+				success:function(object) {
+					console.log("Saved object");
+					alert("You've created an event", function() { document.location.href='index.html'; });
+				},
+				error:function(model, error) {
+					console.log("Please try again later.");
+				}
+			});
+ 
+	});
+
+});
+
+if($("#event").length === 1) {
+ 
+	//Update status to let the user know we are doing important things. Really important.
+	$("#event").html("Checking your location for nearby events.");
+ 
+	navigator.geolocation.getCurrentPosition(function(pos) {
+		var myLocation = new Parse.GeoPoint({latitude: pos.coords.latitude, longitude: pos.coords.longitude});
+ 
+		//Begin query
+		var query = new Parse.Query(TipObject);
+		//Only within 10 miles
+		query.withinMiles("location", myLocation, 10);
+		query.find({
+			success:function(results) { getList(CommentObject,myLocation); },
+			error: function(error) { alert("Error: " + error.code + " " + error.message); }
+		});
+ 
+	}, function(err) {
+		//Since geolocation failed, we can't allow the user to submit
+		alert("Sorry, but we couldn't find your location.");
+	},{timeout:20000,enableHighAccuracy:false});
+}
+
+
+function getList(CommentObject){
+    console.log("getList" + CommentObject);
+    var query = new Parse.Query(CommentObject);
+    query.find({
+        success: function(results) {
+            console.log(results);
+            $.each(results, function( index, value ) {
+            console.log(results[index].attributes.cost);
+            htmlBuilder +=  '<div class="box">' + '<div class="row">' + '<div class="small-10 columns">' + '<ul>' + results[index].attributes.name + '</br>' + results[index].attributes.venue + " : " + results[index].attributes.town + ", " + results[index].attributes.state +  '</br>' + results[index].attributes.day + " | " + results[index].attributes.time + '</br>'
+            + results[index].attributes.cost + '</ul>' + '</div>' + '<button id="element1" onclick="javascript:changeText(1)">Not Going</button>'+ '<div class="small-2 columns">' + '</br></br>' + '<div class="friend-box">' + '<i class="fi-torso">' + " 3" + '</i>' + '</div>' + '</div>' + '</div>' + '</div>' + '</a>';
+});
+            $("#event").html(htmlBuilder);
+        },
+        
+        error: function(error) {
+        }
+	
+	
+        
+    });
+	
+}	
