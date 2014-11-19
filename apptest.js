0633a320159706b48ef147942203a4afc0254a15
kbweeks <kbweeks@kbweeks.att.net>
2014-11-18 22:23:54 -0500

    going works

--- apptest.js
+++ apptest.js
@@ -44,11 +44,13 @@ $(document).ready(function() {
 	
 	
 	
+	
+	
 	if($("#submitEventBtn").length === 1) {
 		currentLocation=null;
-		navigator.geolocation.getCurrentPosition(function(pos) {
+		navigator.geolocation.getCurrentPosition(function(position) {
 			//store the long/lat
-			currentLocation = {longitude:pos.coords.longitude, latitude:pos.coords.latitude};
+			currentLocation = {longitude:position.coords.longitude, latitude:position.coords.latitude};
 			console.log(pos);
 			$("#sumbitEventBtn");
 		}, function(err) {
@@ -134,9 +136,10 @@ function getList(CommentObject){
             $.each(results, function( index, value ) {
             console.log(results[index].attributes.cost);
             htmlBuilder +=  '<div class="box">' + '<div class="row">' + '<div class="small-9 columns">' + '<ul>' + results[index].attributes.name + '</br>' + results[index].attributes.venue + " : " + results[index].attributes.town + ", " + results[index].attributes.state +  '</br>' + results[index].attributes.day + " | " + results[index].attributes.time + '</br>'
-            + results[index].attributes.cost + '</ul>' + '</div>' +'<div class="small-2 columns">'+'<button data-text-swap="Going">Not Going</button>' + '</br>' + '<div class="friend-box">' + '<i class="fi-torso">' + " 3" + '</i>' + '</div>' + '</div>' + '</div>' + '</div>' + '</a>';
+            + results[index].attributes.cost + '</ul>' + '</div>' +'<div class="small-2 columns">'+'<input class="text-swap" value="Not Going" type="button" />' + '</br>' + '<div class="friend-box">' + '<i class="fi-torso">' + '<span class="counter"> 0</span>' + '</i>' + '</div>' + '</div>' + '</div>' + '</div>' + '</a>';
 });
             $("#event").html(htmlBuilder);
+	    buttonClick();
         },
         
         error: function(error) {
@@ -148,13 +151,23 @@ function getList(CommentObject){
 	
 }
 
+function buttonClick(){
+	console.log("button");
+//var button = document.querySelectorAll("button")[0];
 
-var button = document.querySelectorAll("button")[0];
-button.addEventListener('click', function() {
-  if (button.getAttribute("data-text-swap") == button.innerHTML) {
-    button.innerHTML = button.getAttribute("data-text-original");
+$(".text-swap").on( "click", function() {
+	console.log("clicked");
+	var button = $(this);
+	var counter = $(this).siblings(".counter");
+	console.log(counter);
+	console.log(button.attr('value'));
+  if (button.attr('value') == "Not Going") {
+	button.attr('value', 'Going');
   } else {
-    button.setAttribute("data-text-original", button.innerHTML);
-    button.innerHTML = button.getAttribute("data-text-swap");
+	button.attr('value', 'Not Going');
+
   }
-}, false);
+  
+});
+
+}
